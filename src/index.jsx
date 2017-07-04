8fac877d3575df396353c051eb89c4b87243949f
Patrik Marin <patrik.marin@futurice.com>
2017-07-04 15:16:20 +0300

    Move style to style folder.

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-require('./style.scss');
+require('./stylesheets/main_style.scss');
 
 ReactDOM.render((
   <App />
